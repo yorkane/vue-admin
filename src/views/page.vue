801f4e1eb2b00b81b569d572bf54ac36cc80c194
yuankai <yuankai@wasu.com>
2018-05-11 19:06:27 +0800

    fix empty vue component err

--- src/views/page.vue
+++ src/views/page.vue
@@ -2,18 +2,18 @@
   <div style="margin: 20px;">
     <el-card>
       <!--<div slot="header">-->
-        <!--<span>{{$route.params.name}}</span>-->
-        <!--&lt;!&ndash;<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>&ndash;&gt;-->
+      <!--<span>{{$route.params.name}}</span>-->
+      <!--&lt;!&ndash;<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>&ndash;&gt;-->
       <!--</div>-->
       <component :is="comp" :withDialog="noDialog" :visible.sync="show" :model.sync="emptyModel" isEditMode></component>
     </el-card>
     <!--<context-menu class="right-menu"-->
-                  <!--:target="contextMenuTarget"-->
-                  <!--:show="contextMenuVisible"-->
-                  <!--@update:show="(show) => contextMenuVisible = show">-->
-      <!--<a href="javascript:;" @click="">复制</a>-->
-      <!--<a href="javascript:;" @click="">引用</a>-->
-      <!--<a href="javascript:;" @click="">删除</a>-->
+    <!--:target="contextMenuTarget"-->
+    <!--:show="contextMenuVisible"-->
+    <!--@update:show="(show) => contextMenuVisible = show">-->
+    <!--<a href="javascript:;" @click="">复制</a>-->
+    <!--<a href="javascript:;" @click="">引用</a>-->
+    <!--<a href="javascript:;" @click="">删除</a>-->
     <!--</context-menu>-->
   </div>
 </template>
@@ -28,7 +28,7 @@
       // ContextMenu,
       ElCard
     },
-    name: 'empty',
+    name: 'page',
     data() {
       this.$root.$on('IntoMapField', (key, field) => {
         this.$router.push({path: '/model/' + field.Comment + '/' + key});
@@ -44,9 +44,16 @@
     computed: {
       comp() {
         let rt = this.$route.currentRoute || this.$route
-        let cp = _import(rt.params.component)
-        return cp || empty
-
+        let compName = rt.params.component
+        if (compName) {
+          try {
+            let cp = _import(rt.params.component)
+            return cp
+          } catch (e) {
+            console.log(e)
+          }
+        }
+        return empty
       }
     },
     created() {
